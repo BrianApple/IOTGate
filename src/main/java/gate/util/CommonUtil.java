f4db90dd0d1d685258a9cdb145885a44496142fe
yangcheng <1012702024@qq.com>
2019-03-14 14:24:10 +0800

    增加工具类MixAll

--- src/main/java/gate/util/CommonUtil.java
+++ src/main/java/gate/util/CommonUtil.java
@@ -2,6 +2,13 @@ package gate.util;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import io.netty.channel.EventLoopGroup;
 
 public class CommonUtil {
@@ -28,5 +35,62 @@ public class CommonUtil {
 		}
 	}
 	
+	
+	public static Options buildCommandlineOptions(final Options options) {
+		
+		Option opt = new Option("n", true, "gate num");
+        opt.setRequired(true);
+        options.addOption(opt);
+		
+		opt = new Option("p", true, "gate port ,defualt 9811");
+        opt.setRequired(false);
+        options.addOption(opt);
+        
+        
+		
+        opt = new Option("c", false, "open Cluster");
+        opt.setRequired(false);
+        options.addOption(opt);
 
+        opt =new Option("z",  true,
+                    "zk address list, eg: 192.168.18.27:2181,192.168.18.27:2182,192.168.18.27:2183");
+        opt.setRequired(false);
+        options.addOption(opt);
+        /**
+         * 如果未启用zookeeper集群的话则手动传入master地址
+         */
+        opt = new Option("m", true, "master addr:port, eg 127.0.0.1:8888");
+        opt.setRequired(false);
+        options.addOption(opt);
+        
+
+        return options;
+    }
+	/**
+	 * Reference from RocketMQ
+	 * @param appName
+	 * @param args
+	 * @param options
+	 * @param parser
+	 * @return
+	 */
+	 public static CommandLine parseCmdLine(final String appName, String[] args, Options options,
+	            CommandLineParser parser) {
+	        HelpFormatter hf = new HelpFormatter();
+	        hf.setWidth(110);
+	        CommandLine commandLine = null;
+	        try {
+	            commandLine = parser.parse(options, args);
+	            if (commandLine.hasOption('h')) {
+	                hf.printHelp(appName, options, true);
+	                System.out.println("-h参数以及成功被捕获---BRIAN");
+	                return null;
+	            }
+	        }
+	        catch (ParseException e) {
+	            hf.printHelp(appName, options, true);
+	        }
+
+	        return commandLine;
+	    }
 }
---
+++ src/main/java/gate/util/MixAll.java
@@ -0,0 +1,117 @@
+package gate.util;
+
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class MixAll {
+	private MixAll(){
+		throw new AssertionError();
+	}
+	public static List<String> inetAddressList ;
+	static{
+		inetAddressList = getLocalInetAddress();
+	}
+	
+	public static List<String> getLocalInetAddress() {
+        List<String> inetAddressList = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+            while (enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                Enumeration<InetAddress> addrs = networkInterface.getInetAddresses();
+                while (addrs.hasMoreElements()) {
+                    inetAddressList.add(addrs.nextElement().getHostAddress());
+                }
+            }
+        }
+        catch (SocketException e) {
+            throw new RuntimeException("get local inet address fail", e);
+        }
+
+        return inetAddressList;
+    }
+	public static long getPID() {
+	    String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+	    if (processName != null && processName.length() > 0) {
+	        try {
+	            return Long.parseLong(processName.split("@")[0]);
+	        }
+	        catch (Exception e) {
+	            return 0;
+	        }
+	    }
+	
+	    return 0;
+	}
+	/**
+	 * 获取本机的ip
+	 * @return
+	 */
+	public static String localhostName() {
+	    try {
+	        return InetAddress.getLocalHost().getHostAddress();
+	    }
+	    catch (Throwable e) {
+	        throw new RuntimeException(
+	            "InetAddress java.net.InetAddress.getLocalHost() throws UnknownHostException"
+	        		, e);
+	    }
+	}
+	/**
+	 * 内网IP
+	 * @return
+	 * @throws SocketException
+	 */
+	
+	public static String linuxLocalIP() throws SocketException{
+		Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+		InetAddress ip = null;
+		String localHostIP = null;
+		List<InetAddress> cache = new ArrayList<>();
+		while (allNetInterfaces.hasMoreElements()){
+			NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+			System.out.println("::::"+netInterface.getName());
+			Enumeration addresses = netInterface.getInetAddresses();
+			while (addresses.hasMoreElements()){
+				ip = (InetAddress) addresses.nextElement();
+				System.out.println("：：：：：：：：：：："+ip.getHostAddress());
+				if (ip != null && ip instanceof Inet4Address){
+					if(!ip.isLoopbackAddress() && ip.isSiteLocalAddress()){
+						localHostIP = ip.getHostAddress();
+						System.out.println("本机的IPV4 = " + ip.getHostAddress());
+					}else{
+						cache.add(ip);
+					}
+					
+				} else if(ip instanceof Inet6Address){
+					if(!ip.isLoopbackAddress() && ip.isSiteLocalAddress()){
+						localHostIP = ip.getHostAddress();
+						System.out.println("本机的IPV6 = " + ip.getHostAddress());
+					}else{
+						cache.add(ip);
+					}
+				}
+			}
+		}
+		
+		if(localHostIP != null){
+			return localHostIP;
+		}else{
+			for (InetAddress inetAddress : cache) {
+				if(inetAddress.isSiteLocalAddress()){
+					System.out.println("去本地地址ip="+inetAddress.getHostAddress());
+					return inetAddress.getHostAddress();
+				}
+			}
+		}
+		return null;
+	}
+	
+}
