b25873ca32225859c1741e19f950a3df68c8ee04
yangcheng <1012702024@qq.com>
2019-03-15 15:20:25 +0800

    修改单机启动时程序阻塞问题

--- src/main/java/gate/server/Server4Terminal.java
+++ src/main/java/gate/server/Server4Terminal.java
@@ -92,17 +92,19 @@ public class Server4Terminal {
 	public static CommandLine commandLine = null;
 	public static int gatePort = 9811;
 	public static String zkAddr = null;
-	public static List<String> masterAddrs = new ArrayList<>(0);
+	public static List<String> masterAddrs = new ArrayList<>(1);
 	public static CountDownLatch locks = new CountDownLatch(1);
 	public static void main(String[] args) {
-		suitCommonLine(args);
+		boolean isCluster = suitCommonLine(args);
 		initEnvriment();
 		addHook();
 		System.setProperty("org.jboss.netty.epollBugWorkaround", "true");
-		try {
-			locks.await();
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
+		if(isCluster){
+			try {
+				locks.await();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
 		}
 		//启动与终端对接的服务端  因为是阻塞运行 需要开线程启动---后续版本中会变动
 		new Thread(new Runnable() {
@@ -136,16 +138,17 @@ public class Server4Terminal {
 	/**
 	 * 命令行
 	 */
-	public static void suitCommonLine(String[] args){
+	public static boolean suitCommonLine(String[] args){
+		
 		commandLine =
 				 CommonUtil.parseCmdLine("iotGateServer", args, CommonUtil.buildCommandlineOptions(new Options()),
                     new PosixParser());
         if (null == commandLine) {
             System.exit(-1);
         }
-		
+		boolean isCluster = false;
         if(commandLine.hasOption("c") && commandLine.hasOption("z")){
-       	 
+        	isCluster = true;
         	zkAddr = commandLine.getOptionValue("z");
         	new ZKFramework().start(zkAddr);
         }else if (commandLine.hasOption("m")) {
@@ -164,6 +167,7 @@ public class Server4Terminal {
         if(commandLine.hasOption("p")){
         	gatePort = Integer.parseInt(commandLine.getOptionValue("p"));
    	 	}
+        return isCluster;
 	}
 	/**
 	 * 环境初始化  ---目前最还先不用spring管理
